df078ce4d373b0a8a16dc822b086eeb6c6ad4ced
Mashrur Hossain Khan <khanmashrur@yahoo.com>
2016-05-05 21:57:03 +0600

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main ()
+{
+	int a;
+
+	cout<<"Enter the first integer\n";
+	cin>>a;
+
+	int b,sum;
+
+	cout<<"Enter second integer\n";
+	cin>>b;
+
+	sum=a+b;
+	
+	cout<<"sum is "<<sum<<std::endl;
+
+	return 0;
+}
